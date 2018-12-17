5ef057f4cf4f0a7d3d09c57fa6eaf66dc6756549
Reed Law <reedlaw@gmail.com>
2018-12-16 18:41:04 -0800

    12

---
+++ 12.cr
@@ -0,0 +1,100 @@
+in = "initial state: #..#####.#.#.##....####..##.#.#.##.##.#####..####.#.##.....#..#.#.#...###..#..###.##.#..##.#.#.....#
+
+.#.## => #
+.###. => #
+#..#. => .
+...## => .
+#.##. => #
+....# => .
+..##. => #
+.##.. => .
+##..# => .
+.#..# => #
+#.#.# => .
+#.... => .
+.#### => #
+.##.# => .
+..#.. => #
+####. => #
+#.#.. => .
+.#... => .
+###.# => .
+..### => .
+#..## => #
+...#. => #
+..... => .
+###.. => #
+#...# => .
+..#.# => #
+##... => #
+##.## => .
+##.#. => .
+##### => .
+.#.#. => #
+#.### => #"
+
+test = "initial state: #..#.#..##......###...###
+
+...## => #
+..#.. => #
+.#... => #
+.#.#. => #
+.#.## => #
+.##.. => #
+.#### => #
+#.#.# => #
+#.### => #
+##.#. => #
+##.## => #
+###.. => #
+###.# => #
+####. => #"
+
+rules = Hash(String, String).new
+state = ""
+in.each_line do |l|
+  if l =~ /^initial state: ([\.#]+)$/
+    state = $1
+  end
+  next if l == ""
+  if l =~ /^([\.#]{5}) => ([\.#]{1})$/
+    rules[$1] = $2
+  end
+end
+
+sum = 0
+index = 0
+20.times do |g|
+  state = grow(state, rules)
+  index -= 2
+end
+
+state.chars.each_with_index do |c, i|
+  pos = index + i
+  sum += pos if c == '#'
+end
+p sum
+
+def grow(state, rules)
+  slices = state.chars.map_with_index do |c, i|
+    if i < 5
+      "."*(4-i) + state[0,i+1]
+    elsif i == state.size
+      state[i-4,1] + "." * 4
+    else
+      state[i-4, 5]
+    end
+  end
+  4.times do |i|
+    n = 4-i # 4
+    slices << state[state.size-n,n] + "."*(i+1)
+  end
+  result = slices.map_with_index do |slice, i|
+    if rules.has_key?(slice)
+      rules[slice]
+    else
+      '.'
+    end
+  end.join
+  result
+end
